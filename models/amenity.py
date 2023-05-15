199a64bcffb26c4d4c5de4b35a3ee74a7d056e52
meseret1918 <meseret1918@gmail.com>
2023-05-15 11:11:02 -0700

    AirBnB

--- models/amenity.py
+++ models/amenity.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-""" shebang line - defines where the interpreter is located """
+"""[models/amenity module]"""
 from models.base_model import BaseModel
-""" import moduls """
 
 
 class Amenity(BaseModel):
-    """ Main class to manage Amenities, it inherited of BaseModel """
+    """[Amenity class]
+    inherits from BaseModel"""
     name = ""
